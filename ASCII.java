c33e04f0d05d23d045a961a1f9983522332b5852
tameemmohammad <mohammadtameem146@gmail.com>
2025-03-01 07:16:11 +0530

    ASCII.java

---
+++ ASCII.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main
+
+{
+
+	public static void main(String[] args) {
+
+	    char ch='A';
+
+	    int castAscii=(int) ch;
+
+	   System.out.println("The Ascii value of"+ch+"is:"+castAscii);
+
+	}
+
+}
