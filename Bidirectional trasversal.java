89c848261873779cdc77db0027248efee4cb8d96
tameemmohammad <mohammadtameem146@gmail.com>
2025-03-01 07:18:56 +0530

    Bidirectional trasversal.java

---
+++ Bidirectional trasversal.java
@@ -0,0 +1,15 @@
+class Main {
+
+    public static void main(String[] args) {
+
+        for(int i=1,j=499;i<=j;i++,j--){
+
+            System.out.println(i);
+
+            System.out.println(j);
+
+        }
+
+    }
+
+}
