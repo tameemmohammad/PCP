6c63ea4ee162e1df6d4e622278e6f999a5fd56ba
tameemmohammad <mohammadtameem146@gmail.com>
2025-03-01 07:29:54 +0530

    Random.java

---
+++ Random.java
@@ -0,0 +1,15 @@
+public class Main{
+    public static void main(String[] args){
+        int c=5;
+        for (int i=10;i<=15;i++)
+        {
+            for(int j=100;j<=103;j++)
+            {
+                c=c+2;
+                
+            }
+        c=c+5;
+        }
+        System.out.println(c);
+    }
+}
